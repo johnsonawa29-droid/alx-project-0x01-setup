85ee0f74c76fcd838ccfbc050f39f1ee992057ec
johnson <johnsonawa29@gmail.com>
2025-09-22 19:21:33 +0100

    yes

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,31 +1,47 @@
-
 import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
 interface PostsPageProps {
   posts: PostProps[];
 }
 
 const Posts: React.FC<PostsPageProps> = ({ posts }) => {
-  console.log(posts)
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [allPosts, setAllPosts] = useState<PostProps[]>(posts);
+
+  const handleAddPost = (newPost: PostData) => {
+    setAllPosts([...allPosts, { ...newPost, id: allPosts.length + 1 }]);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
         <div className="flex justify-between">
-          <h1 className=" text-2xl font-semibold">Post Content</h1>
-          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add Post
+          </button>
         </div>
-        <div className="grid grid-cols-3 gap-2 ">
-          {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+        <div className="grid grid-cols-3 gap-2">
+          {allPosts?.map(({ title, body, userId, id }: PostProps, key: number) => (
             <PostCard title={title} body={body} userId={userId} id={id} key={key} />
           ))}
         </div>
       </main>
+
+      {isModalOpen && (
+        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      )}
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
