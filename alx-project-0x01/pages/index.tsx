2b8d2b5462b9202c50ffd94306329bbc0d2156c4
johnson <johnsonawa29@gmail.com>
2025-09-22 21:09:21 +0100

    your commit message

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,14 +1,10 @@
-
 import React from "react";
-import Header from "../components/layout/Header";
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <Header />
-      <div className="flex justify-center items-center h-screen">
-        <h1 className="text-7xl font-thin">Welcome to our Application!</h1>
-      </div>
+    <div className="flex justify-center items-center h-screen">
+
+      <h1 className="text-7xl font-thin">Welcome  to  our  Application!</h1>
     </div>
   );
 };
