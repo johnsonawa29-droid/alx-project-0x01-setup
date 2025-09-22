90a7000f50f63138ccf35274f105c4a5e0839bee
johnson <johnsonawa29@gmail.com>
2025-09-22 15:36:15 +0100

    yes

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-200 text-center p-4 mt-10">
+      <p className="text-gray-600">&copy; 2025 My App</p>
+    </footer>
+  );
+};
+
+export default Footer;
