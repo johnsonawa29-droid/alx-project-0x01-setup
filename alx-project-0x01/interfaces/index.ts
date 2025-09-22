e09a8765e572a34c7764b7eb5a38b303a1c952a4
johnson <johnsonawa29@gmail.com>
2025-09-22 19:12:52 +0100

    yes

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,36 +1,12 @@
-export interface PostProps {
+
+export interface PostData {
   userId: number;
-  id: number;
-  title: string;
-  body: string;
-}
-export interface PostProps {
-  userId: number;
-  id: number;
+  id?: number;
   title: string;
   body: string;
 }
 
-export interface UserProps {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: string;
-      lng: string;
-    };
-  };
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  };
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
 }
