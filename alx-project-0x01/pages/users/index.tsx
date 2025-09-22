e48a9e2104373716588b230da35d59dfb3a66224
johnson <johnsonawa29@gmail.com>
2025-09-22 15:55:42 +0100

    yes

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import Button from "../../components/common/Button";
 
 const Users: React.FC = () => {
   return (
     <div className="p-6">
-      <h1 className="text-3xl mb-4">Users</h1>
-      <Button label="Add User" onClick={() => alert("User added!")} />
+      <h1 className="text-3xl mb-4">Users Page</h1>
+      <p>Here we will list our users.</p>
     </div>
   );
 };
